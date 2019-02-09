65619d2b7deaea79d04529467a7e683e85447c64
tobetobetong <rin090506@ezweb.ne.jp>
2019-02-09 17:47:49 +0900

    "yyyy/MM/dd"からスラッシュを除きました。

--- src/main/java/jp/co/tis/tiscon4/core/validation/validator/YYYYMMDD.java
+++ src/main/java/jp/co/tis/tiscon4/core/validation/validator/YYYYMMDD.java
@@ -16,7 +16,7 @@ import static java.lang.annotation.RetentionPolicy.RUNTIME;
 /**
  * 入力値が指定した形式の日付文字列であるかをバリデーションするクラス。
  * <p/>
- * 日付フォーマットのデフォルト値は「yyyy/MM/dd」である。
+ * 日付フォーマットのデフォルト値は「yyyyMMdd」である。
  *
  * @author Indo Yoshihiro
  *
